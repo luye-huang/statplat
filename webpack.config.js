c6d867a4f5b01e6f51fc184e5dabd98cf3d7c325
huangluye <huangluye@wanda.cn>
2017-03-03 21:43:34 +0800

    plugin works

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
-// var debug = process.env.NODE_ENV !== "production";
-var debug = process.traceDeprecation = true
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: __dirname + '/src',
-  // devtool: debug ? "inline-sourcemap" : null,
-  // entry: "./js/index.js",
-  entry: [
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      './js/index.js'
-    ],
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/js/index.js",
   module: {
     loaders: [
       {
@@ -19,21 +13,18 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
-          // plugins: ['react-html-attrs'], //添加组件的插件配置
+          presets: ['react', 'es2015']
         }
-      },
-      //下面是使用 ant-design 的配置文件
-      // { test: /\.css$/, loader: 'style-loader!css-loader' }
+      }
     ]
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "bundle.js"
+    path: __dirname,
+    filename: "./src/bundle.js"
   },
-  // plugins: debug ? [] : [
-  //   new webpack.optimize.DedupePlugin(),
-  //   new webpack.optimize.OccurenceOrderPlugin(),
-  //   new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  // ],
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
