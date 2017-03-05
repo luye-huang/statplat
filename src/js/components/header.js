834c42eb1b4e7c486baae139ab1a0c4acf139f10
huangluye <huangluye@wanda.cn>
2017-03-05 09:36:16 +0800

    add a component

---
+++ src/js/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+export default class ComponentHeader extends React.Component{
+  render(){
+    return (
+      <header>
+        <h1>this is header</h1>
+      </header>
+    )
+  }
+}
