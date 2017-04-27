8559ddbbcc6521fea0468f8788a7208e6acf4c06
huangluye <huangluye@wanda.cn>
2017-04-27 15:44:41 +0800

    slight modifications

--- src/js/components/essence1.js
+++ src/js/components/essence1.js
@@ -3,27 +3,30 @@
  */
 import React from 'react';
 import LuyeTable from './luyeTable/luyeTable';
-export default class Essence1 extends React.Component{
-  render(){
-    return(
-      <div><div id="tb-div"></div></div>
+export default class Essence1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="tb-div"></div>
+      </div>
     )
   }
-  componentDidMount(){
+
+  componentDidMount() {
     console.log('did mount!!');
-    var cb = (e)=>{
+    var cb = (e)=> {
       console.log(e.data);
     }
-    const adjustColor = (param)=>{
+    const adjustColor = (param)=> {
       const val = Number.parseInt(param.text());
-      if(val>60)param.css('background-color', 'yellow');
+      if (val > 60)param.css('background-color', 'yellow');
     }
     let tb = new LuyeTable({
-      el:document.getElementById('tb-div'),
-      columns : [{cname : '团队名称', cdata : 'tname', action:'click', trigger:cb},
-        {cname : '代码行总数', cdata : 'code_count', style:'hide'},
-        {cname : '提交文件总数', cdata : 'file_count'},
-        {cname : '团队总得分', cdata : 'tscore', adjust: adjustColor}]
+      el: document.getElementById('tb-div'),
+      columns: [{cname: '团队名称', cdata: 'tname', action: 'click', trigger: cb},
+        {cname: '代码行总数', cdata: 'code_count', style: 'hide'},
+        {cname: '提交文件总数', cdata: 'file_count'},
+        {cname: '团队总得分', cdata: 'tscore', adjust: adjustColor}]
     })
   }
 }
